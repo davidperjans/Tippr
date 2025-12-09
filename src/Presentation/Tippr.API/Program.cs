0f22f6c526a7b2482f5af1c6fc721f2c592f399f
David Perjans <perjansdavid@gmail.com>
2025-12-09 12:46:49 +0100

    extended swagger docs support with authorize button

--- src/Presentation/Tippr.API/Program.cs
+++ src/Presentation/Tippr.API/Program.cs
@@ -1,6 +1,7 @@
-using Tippr.Infrastructure;
-using Tippr.Application;
+using Microsoft.OpenApi.Models;
 using Serilog;
+using Tippr.Application;
+using Tippr.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +17,52 @@ builder.Services.AddApplicationServices();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Tippr API",
+        Version = "v1",
+        Description = "API för Tippr",
+        Contact = new OpenApiContact
+        {
+            Name = "David Perjans",
+            Email = "perjansdavid@gmail.com"
+        }
+    });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Klistra in din JWT-token nedan (utan 'Bearer ')",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
